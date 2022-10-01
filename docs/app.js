4cd98e4d4d399e19109d5f4d25616d33b0213dc8
Nick <nicolakazakov@gmail.com>
2022-10-01 18:22:17 +0300

    Game V1

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,258 @@
 document.body.addEventListener('touchstart', function(e){ e.preventDefault(); });
+
+// DATABASE INDEXEDDB
+
+const indexedDB =
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
+
+const request = indexedDB.open("UsernameData", 1);
+
+request.onerror = function (event) {
+    console.error("An error occurred with IndexedDB");
+    console.error(event);
+}
+
+request.onupgradeneeded = function () {
+    const db = request.result;
+    const store = db.createObjectStore("username", { keyPath: "id" });
+    store.createIndex("un", ["unstring"], {unique: true});
+}
+
+//DB TURNED OFF
+/*request.onsuccess = function () {
+    const db = request.result;
+    const transaction = db.transaction("username", "readwrite");
+
+    const store = transaction.objectStore("username");
+    const userIndex = store.index("un");
+
+    const idQuery = store.get(1);
+
+    idQuery.onsuccess = function () {
+        console.log('Username: ', idQuery.result);
+        if(idQuery.result == undefined) {
+            console.log("No username.");
+            var welcome = document.getElementById("welcome-content");
+            var home = document.getElementById("home-content");
+            welcome.style.display = "flex";
+            home.style.display = "none";
+        } else {
+            console.log("Username found!");
+            var welcome = document.getElementById("welcome-content");
+            var home = document.getElementById("home-content");
+            welcome.style.display = "none";
+            home.style.display = "flex";
+        }
+    }
+}*/
+
+function remember () {
+    const db = request.result;
+    const transaction = db.transaction("username", "readwrite");
+    const store = transaction.objectStore("username");
+
+    const unval = document.getElementById("username-input").value;
+
+    store.put({ id: 1, unstring: unval });
+
+    var welcome = document.getElementById("welcome-content");
+    var home = document.getElementById("home-content");
+    welcome.style.display = "none";
+    home.style.display = "flex";
+}
+
+play();
+
+function play () {
+    game();
+}
+
+function game () {
+    const canvas = document.getElementById("halloween-game-canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth - 20;
+    canvas.height = window.innerHeight - 20;
+
+    // START POSITION BASKET
+    fingerX = (canvas.width)/2 - 50;
+    // ARR
+    let candies = [];
+    let bones = [];
+
+    document.addEventListener("touchstart", e => {
+        fingerX = e.changedTouches[0].pageX;
+        fingerY = e.changedTouches[0].pageY;
+        fingerX = fingerX - 50;
+    })
+
+    document.addEventListener("touchmove", e => {
+        fingerX = e.changedTouches[0].pageX;
+        fingerY = e.changedTouches[0].pageY;
+        fingerX = fingerX - 50;
+    })
+
+
+    class Candy {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 50;
+            this.height = 50;
+            this.x = rng(10, 290);
+            this.y = 0;
+            this.markedForDeletion = false;
+            this.image = document.getElementById("corn");
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+        update(){
+            this.y += rng(3,10);
+            if(this.y > this.gameHeight + 100) this.markedForDeletion = true;
+        }
+    }
+
+    class Bone {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 70;
+            this.height = 70;
+            this.x = rng(10, 290);
+            this.y = 0;
+            this.markedForDeletion = false;
+        }
+        draw(context){
+            context.fillStyle = "red";
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+        update(context){
+            this.y += rng(3,10);
+            if(this.y > this.gameHeight + 100) this.markedForDeletion = true;
+        }
+    }
+
+
+    function handleObjects (deltaTime) {
+
+        if(candyTimer > candyInterval){
+            candies.push(new Candy(canvas.width, canvas.height));
+            candyTimer = 0;
+        } else {
+            candyTimer += deltaTime;
+        }
+
+        candies.forEach(candy => {
+            candy.draw(ctx);
+            candy.update(ctx);
+        });
+        candies = candies.filter(candy => !candy.markedForDeletion);
+
+        if(boneTimer > boneInterval){
+            bones.push(new Bone(canvas.width, canvas.height));
+            boneTimer = 0;
+        } else {
+            boneTimer += deltaTime;
+        }
+
+        bones.forEach(bone => {
+            bone.draw(ctx);
+            bone.update(ctx);
+        });
+        bones = bones.filter(bone => !bone.markedForDeletion);
+    }
+
+    class Player {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 100;
+            this.height = 100;
+            this.x = (this.gameWidth)/2 - (this.width)/2;
+            this.y = this.gameHeight - this.height - 80;
+            this.image = document.getElementById("cauldron");
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            //context.fillRect(this.x, this.y, this.width, this.height);
+        }
+        update(candies, bones){
+            this.x = (lerp (this.x, fingerX, 0.05));
+
+            if(this.x > 295){
+                this.x = 295;
+            } else if (this.x < 0){
+                this.x = 0;
+            }
+
+            // CANDIES, BONES
+            candies.forEach(candy => {
+                const dx = candy.x - this.x;
+                const dy = candy.y - this.y;
+                const distance = Math.sqrt(dx*dx + dy*dy);
+                if(distance < candy.width/2 + this.width/2){
+                    if(candyLock == false){
+                        candyLock = true;
+                        score++;
+                        console.log("Collected Candy!");
+                        document.getElementById("score").innerHTML = score.toString();
+                        candies
+                    }
+                } else {
+                    candyLock = false;
+                }
+            });
+
+            bones.forEach(bone => {
+                const dx = bone.x - this.x;
+                const dy = bone.y - this.y;
+                const distance = Math.sqrt(dx*dx + dy*dy);
+                if(distance < bone.width/2 + this.width/2){
+                    console.log("Collected Bone!");
+                }
+                /*if(this.x + this.width >= bone.x && this.x <= bone.x + bone.width && this.y + this.height >= bone.y && this.y <= bone.y + bone.height) {
+                        if(boneLock == false){
+                            console.log("Collected Bone!");
+                            boneLock = true;
+                        }
+                    } else {
+                        boneLock = false;
+                    }*/
+            });
+        }
+    }
+
+    function rng(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    function lerp (start, end, amt) {
+        return (1-amt)*start+amt*end
+    }
+
+    const player = new Player(canvas.width, canvas.height);
+
+    let lastTime = 0;
+    let candyTimer = 0;
+    let candyInterval = rng(500, 1200);
+    let boneTimer = 0;
+    let boneInterval = rng(700, 900);
+    let gameOver = false;
+    let score = 0;
+    let candyLock = false;
+
+    function animate (timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        handleObjects(deltaTime);
+        player.draw(ctx);
+        player.update(candies, bones);
+        if (!gameOver) requestAnimationFrame(animate);
+    }
+    animate(0);
+}
