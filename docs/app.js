0110678e0885bbda7217cd8fc5bd87789d6e41fc
Nick <nicolakazakov@gmail.com>
2022-10-02 05:20:30 +0300

    update

--- docs/app.js
+++ docs/app.js
@@ -145,13 +145,13 @@ function remember () {
 
 function play () {
     game();
-    dice = rng(1,10);
+    /*dice = rng(1,10);
     console.log(dice);
     if(dice <= 2){
         document.body.style.backgroundImage = "url('background.png')";
     } else {
         document.body.style.backgroundImage = "url('background-original.png')";
-    }
+    }*/
 }
 
 function rng(min, max) {
